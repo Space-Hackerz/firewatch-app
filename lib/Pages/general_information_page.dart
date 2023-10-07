2b44712a53f9657ba940fcdafbe2fe5f95531fe2
TosnEula <dominiqueshim9999@gmail.com>
2023-10-07 16:54:07 +0200

    Created pages

---
+++ lib/Pages/general_information_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GeneralInformation extends StatelessWidget {
+  const GeneralInformation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        body: Text("general info")
+    );
+  }
+}
