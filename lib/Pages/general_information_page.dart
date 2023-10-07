10dadd55fd7a1042c041aa00c8a055c3f9c7d306
Andy Ponce <andy.ponce@outlook.com>
2023-10-07 13:12:04 -0400

    I made buttons at the bottom that are centered...yaaaay

--- lib/Pages/general_information_page.dart
+++ lib/Pages/general_information_page.dart
@@ -6,8 +6,35 @@ class GeneralInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        body: Text("general info")
+      //App bar
+      appBar: AppBar(
+        title: const Text("General Info",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body:Center(
+        child:Column(
+          children: <Widget>[
+            const SizedBox(height: 400),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Emergency Response Info'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Evacuation Route Information'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Safety 101'),
+            )
+          ],
+        ),
+      )
     );
   }
 }
