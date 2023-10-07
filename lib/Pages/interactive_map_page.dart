f1812b8fd0e1f3b3e4230dd87185708bf885a6bb
TosnEula <dominiqueshim9999@gmail.com>
2023-10-07 16:21:45 +0200

    Created pages

---
+++ lib/Pages/interactive_map_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InteractiveMap extends StatelessWidget {
+  const InteractiveMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/Pages/login_screen_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //App bar
+      appBar: AppBar(
+        title: const Text("Firewatch",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            ),
+        ),
+        centerTitle: true,
+      ),
+
+      //Body
+      body: Column(
+        children: <Widget>[
+          //Image
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: 255,
+            child: Image.asset(""),//Need image of hilary clinton
+          ),
+          //spacing
+          SizedBox(height: 40,),
+
+          //Form information
+          Padding(padding: EdgeInsets.all(20),
+          child: Container(
+            decoration: const BoxDecoration(
+              //TODO change colour of box if needed
+              color: Colors.white24,
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 8,
+                  color: Colors.black26,
+                  offset: Offset(0,-3),
+                ),
+              ]
+            ),
+
+
+            ),
+          ),
+        ],
+      ),
+
+    );
+  }
+}
