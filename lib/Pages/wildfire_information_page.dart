3ca43acc4a36c7620d90b26b27c92f18a3d8d95f
TosnEula <dominiqueshim9999@gmail.com>
2023-10-08 04:41:40 +0200

    Outline of wildfire information complete.

--- lib/Pages/wildfire_information_page.dart
+++ lib/Pages/wildfire_information_page.dart
@@ -5,6 +5,84 @@ class WildfireInformation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    String fireSrc = "";
+    String fireArea = "0km";
+
+    List<String> locations = [
+      "Califonia",
+      "Madagascar",
+      "Hawaii"
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF851229),
+        title: const Text("Wildfire Information",
+          style: TextStyle(
+            color: Color(0xFFd0d2d6),
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Source of fire: $fireSrc",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+             ),
+            ),
+
+            const SizedBox(height: 10),
+
+            Text("Area: $fireArea",
+              style: TextStyle(
+                fontSize: 25,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            const SizedBox(height: 10),
+
+
+            Center(
+              child: Text("Affected areas",
+                style: TextStyle(
+                  decoration: TextDecoration.underline,
+                  fontSize: 50,
+              ),
+              ),
+            ),
+            const SizedBox(height: 5),
+
+            ListView.builder(
+              itemCount: locations.length,
+              shrinkWrap: true, itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                   padding: EdgeInsets.all(15),
+                    child: Text(locations[index],
+                      style: TextStyle(
+                      fontSize: 15,
+                        )
+                      )
+              );
+            },
+            )
+
+
+
+          ],
+        ),
+      ),
+
+    );
   }
 }
