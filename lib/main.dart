e0ef17c0cf531e2c6a1690f634e9fc1e3c2fdcff
TosnEula <dominiqueshim9999@gmail.com>
2023-10-08 03:13:22 +0200

    Report wildfire design complete.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firewatch/Pages/general_information_page.dart';
 import 'package:firewatch/Pages/interactive_map_page.dart';
 import 'package:firewatch/Pages/login_screen_page.dart';
 import 'package:firewatch/Pages/wildfire_information_page.dart';
+import 'package:firewatch/Pages/report_wildfire_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -11,12 +12,13 @@ void main() => runApp(MaterialApp(
         seedColor: const Color(0xFF851229))
   ),
 
-  initialRoute: '/GeneralInfo',
+  initialRoute: '/WildfireRep',
   routes: {
     '/': (context) => LoginScreen(),
     '/WildfireInfo': (context) => WildfireInformation(),
     '/InterMap': (context) => InteractiveMap(),
     '/GeneralInfo': (context) => GeneralInformation(),
+    '/WildfireRep' : (context) => WildfireReport(),
 
   },
 ));
